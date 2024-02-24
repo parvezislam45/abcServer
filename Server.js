0bff6900fb8f5acd73b3768366cbb16f9d0e7949
parvezislam45 <parvezislam45@gmail.com>
2024-02-24 21:46:05 +0600

    final

--- Server.js
+++ Server.js
@@ -3,18 +3,32 @@ const app = express();
 const port = process.env.PORT || 9000;
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const cors = require("cors");
+const multer = require('multer');
+const path = require('path')
+const fs = require('fs');
 
 app.use(cors());
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads') // specify the upload directory
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+});
 
+const upload = multer({ storage: storage });
 const uri = "mongodb+srv://aurthohinparvez:2tdBD4q1zQnAt1ug@cluster0.4scykru.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run (){
     try{
         await client.connect();
-        const productCollection = client.db("products").collection("product");
-        const userCollection = client.db("userCollection").collection("user")
+        const productCollection = client.db("productCollection").collection("product");
+        const userCollection = client.db("userCollection").collection("user");
+        const orderCollection = client.db("orderCollection").collection("order");
 
         app.get("/product", async (req, res) => {
             const query = {};
@@ -40,32 +54,51 @@ async function run (){
             res.send(result);
           });
 
-        app.post("/product", async (req, res) => {
-            const newProduct = req.body;
-            const result = await productCollection.insertOne(newProduct);
-            res.send(result);
-          });
-
-
-          app.put("/product/:id", async (req, res) => {
-            const id = req.params.id;
-            const data = req.body;
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = { $set: {} };
-            if (data.name) updateDoc.$set.name = data.name;
-            if (data.description) updateDoc.$set.description = data.description;
-            if (data.price) updateDoc.$set.price = data.price;
-            if (data.category) updateDoc.$set.category = data.category;
-            if (data.image) updateDoc.$set.image = data.image;
-        
+          app.post("/product", upload.single('image'), async (req, res) => {
             try {
-                const result = await productCollection.updateOne(filter, updateDoc);
-                res.send(result);
+              const newProduct = {
+                name: req.body.name,
+                price: req.body.price,
+                description: req.body.description,
+                image: req.file.filename // Assuming you're saving the filename in the database
+              };
+          
+              const result = await productCollection.insertOne(newProduct);
+              res.send(result);
             } catch (error) {
-                console.error("Error updating product:", error);
-                res.status(500).send("Error updating product");
+              console.error('Error:', error);
+              res.status(500).send('Server Error');
             }
-        });
+          });
+          
+
+          app.put("/product/:id", upload.single('image'), async (req, res) => {
+            try {
+              const id = req.params.id;
+              const data = req.body;
+              const filter = { _id: new ObjectId(id) };
+              const updateDoc = { $set: {} };
+          
+              if (data.name) updateDoc.$set.name = data.name;
+              if (data.description) updateDoc.$set.description = data.description;
+              if (data.price) updateDoc.$set.price = data.price;
+              if (req.file) updateDoc.$set.image = req.file.filename; // Update image only if a new image is provided
+          
+              // Fetch the existing product to get the old image filename
+              const existingProduct = await productCollection.findOne(filter);
+          
+              // If a new image is provided and there's an existing image, delete the old image
+              if (req.file && existingProduct.image) {
+                fs.unlinkSync(path.join(__dirname, 'uploads', existingProduct.image)); // Delete old image file
+              }
+          
+              const result = await productCollection.updateOne(filter, updateDoc);
+              res.send(result);
+            } catch (error) {
+              console.error("Error updating product:", error);
+              res.status(500).send("Error updating product");
+            }
+          });
 
         app.delete("/product/:id", async (req, res) => {
             const id = req.params.id;
@@ -73,6 +106,9 @@ async function run (){
             const result = await productCollection.deleteOne(query);
             res.send(result);
           });
+
+
+          // -------------------------- User ---------------------------------
 
           app.post("/user", async (req, res) => {
             const newUser = req.body;
@@ -85,48 +121,61 @@ async function run (){
             res.send(users);
           });
 
-          app.get("/admin/:email", async (req, res) => {
-            try {
-                const email = req.params.email;
-                const user = await userCollection.findOne({ email: email });
-        
-                if (user) {
-                    const isAdmin = user.role === "admin";
-                    res.send({ admin: isAdmin });
-                } else {
-                    // If no user is found with the specified email
-                    res.status(404).send({ message: "User not found" });
-                }
-            } catch (error) {
-                console.error("Error:", error);
-                res.status(500).send({ message: "Internal Server Error" });
-            }
-        });
-        
-      
-          app.put("/user/admin/:email", async (req, res) => {
-            const email = req.params.email;
-        
-            // Allow unauthenticated requests (for educational purposes only)
-            // In a real-world scenario, you should implement proper authentication and authorization logic here.
-        
-            // Your server-side logic to make a user an admin goes here...
-        
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: "admin" },
-            };
-        
-            const result = await userCollection.updateOne(filter, updateDoc);
-        
-            if (result.modifiedCount > 0) {
-                res.send({ modifiedCount: result.modifiedCount, message: "Successfully made an admin" });
-            } else {
-                res.status(403).send({ message: "Failed to make an admin" });
-            }
-        });
-        
-        
+      app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+
+
+    // ----------------------- Order ---------------------------
+    app.post("/orders", async (req, res) => {
+      const {userName, productName, email, price } = req.body; // Extracting specific fields
+      const newItem = {userName, productName, email, price }; // Creating an object with specific fields
+      const result = await orderCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+
+    app.get("/orders", async (req, res) => {
+      const orders = await orderCollection.find().toArray();
+      res.send(orders);
+    });
+     
+    app.get("/orders/:id", async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const orders = await orderCollection.findOne(query);
+      res.send(orders)
+    })
+
+
+    app.put("/orders/:id", async (req, res) => {
+      try {
+          const id = req.params.id;
+          const data = req.body;
+          const filter = { _id: new ObjectId(id) };
+          const updateDoc = { $set: {} };
+          if (data.price) updateDoc.$set.price = data.price;
+  
+          const result = await orderCollection.updateOne(filter, updateDoc);
+          res.send(result);
+      } catch (error) {
+          console.error("Error updating order:", error);
+          res.status(500).send("Error updating order");
+      }
+  });
+
+
+    app.get("/orders/user/:email", async (req, res) => {
+      const email=req.params.email
+      const query={email:email}
+      const users = await orderCollection.find(query).toArray();
+      res.send(users);
+    });
+
     }
     finally{}
 }
